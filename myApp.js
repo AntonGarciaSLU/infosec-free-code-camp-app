90c27c07ac35ab0be359dac3e3a255c20633959b
Anton Garcia <2230132@slu.edu.ph>
2025-09-01 01:00:06 +0800

    4

--- myApp.js
+++ myApp.js
@@ -1,70 +1,33 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
-const helmet = require("helmet");  
-
+// Hide "X-Powered-By" header
 app.use(helmet.hidePoweredBy());
 
+// Mitigate clickjacking
+app.use(
+  helmet.frameguard({
+    action: 'deny',
+  })
+);
+
+// Serve static files
 app.use(express.static('public'));
 
-app.get("/", function (req, res) {
+// Root route
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // Required export for FCC tests
 module.exports = app;
 
-app.use(helmet());     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+// API routes (FCC tests)
 const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+
+// Start server
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
