315ce73a23503e01f131380f4ea22ff005392e22
Anton Garcia <2230132@slu.edu.ph>
2025-09-01 18:54:43 +0800

    helmet.ieNoOpen

--- myApp.js
+++ myApp.js
@@ -15,6 +15,9 @@ app.use(
 // Prevent MIME type sniffing
 app.use(helmet.noSniff());
 
+// Prevent IE from executing downloads in site's context
+app.use(helmet.ieNoOpen());
+
 app.use(helmet.xssFilter());
 
 // Serve static files
@@ -22,7 +25,7 @@ app.use(express.static('public'));
 
 // Root route
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html'); 
 });
 
 // Required export for FCC tests
